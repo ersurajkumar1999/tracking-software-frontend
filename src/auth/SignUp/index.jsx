66470a4dafef616af3bf95338d6781a1a0bf3e3d
ersurajkumar1999 <er.surajkumar1999@gmail.com>
2024-07-05 00:20:52 +0530

    rigster page done

--- src/auth/SignUp/index.jsx
+++ src/auth/SignUp/index.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+
+// material-ui
+import { Grid, Stack, Typography } from '@mui/material';
+import AuthWrapper from 'auth/componentes/AuthWrapper';
+import SignUpPage from 'auth/componentes/SignUpPage';
+
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <AuthWrapper>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+            <Typography variant="h3">Sign up</Typography>
+            <Typography component={Link} to="/login" variant="body1" sx={{ textDecoration: 'none' }} color="primary">
+              Already have an account?
+            </Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <SignUpPage />
+        </Grid>
+      </Grid>
+    </AuthWrapper>
   )
 }
 
