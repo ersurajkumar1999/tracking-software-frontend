8a1af4bb17269c501056c3da698e8139115d946d
ersurajkumar1999 <er.surajkumar1999@gmail.com>
2024-07-05 08:56:39 +0530

    sidebar update

---
+++ src/routes/UserRoutes.jsx
@@ -0,0 +1,53 @@
+import { lazy } from 'react';
+
+// project import
+import Loadable from 'components/Loadable'
+import UserLayout from 'user/layout';
+
+const Color = Loadable(lazy(() => import('pages/component-overview/color')));
+const Typography = Loadable(lazy(() => import('pages/component-overview/typography')));
+const Shadow = Loadable(lazy(() => import('pages/component-overview/shadows')));
+const DashboardDefault = Loadable(lazy(() => import('pages/dashboard/index')));
+
+// render - sample page
+const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const UserRoutes = {
+  path: '/',
+  element: <UserLayout />,
+  children: [
+    {
+      path: 'user',
+      children: [
+        {
+          path: 'dashboard',
+          element: <DashboardDefault />
+        },
+        {
+          path: 'profile',
+          element: <SamplePage />
+        },
+        {
+          path: 'sample-page',
+          element: <SamplePage />
+        },
+      ]
+    },
+    // {
+    //   path: 'sample-page',
+    //   element: <SamplePage />
+    // },
+    // {
+    //   path: 'shadow',
+    //   element: <Shadow />
+    // },
+    // {
+    //   path: 'typography',
+    //   element: <Typography />
+    // }
+  ]
+};
+
+export default UserRoutes;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 // project import
-import MainRoutes from './MainRoutes';
+// import MainRoutes from './MainRoutes';
 import LoginRoutes from './LoginRoutes';
+import UserRoutes from './UserRoutes';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
-const router = createBrowserRouter([MainRoutes, LoginRoutes]);
+const router = createBrowserRouter([UserRoutes, LoginRoutes]);
 
 export default router;
